fc6e75e38536eea129adeabe3bdc972b64a90368
justallyt <albertjunk2030@gmail.com>
2025-05-29 15:01:49 +0300

    updates

--- src/components/home/SolutionSection.jsx
+++ src/components/home/SolutionSection.jsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom"
 import dummy from "../../assets/dashboard2.png"
 import { GoArrowUpRight } from "react-icons/go";
 import FaqSection from "./FaqSection";
+import { useState } from "react";
+import { CgClose } from "react-icons/cg";
 
 const SolutionSection = () => {
+  const [ modalActive, setModalActive ] = useState(true)
   return (
     <div className='solution-section'>
              <div className="inner-row">
@@ -17,11 +20,18 @@ const SolutionSection = () => {
                                             <div className="solution-overlay">
                                                         <div className="overlay-texts">
                                                                  <h3>Explore Erebus in Action</h3>
-                                                                 <Link to={"/"}>Play Demo <span><GoArrowUpRight /></span></Link>
+                                                                 <Link to={"/"} onClick={() => setModalActive(true)}>Play Demo <span><GoArrowUpRight /></span></Link>
                                                         </div>
                                             </div>
                                               <img src={dummy} alt="" />
                                    </div>
+
+                                       <div  className={ modalActive ? 'solution-video active' : 'solution-video'}>
+                                                <span className="video-close-btn" onClick={()=> setModalActive(false)}><CgClose /></span>
+                                              <div className="video-body">
+                                                      <iframe src={modalActive ? "https://res.cloudinary.com/dq6subhaj/video/upload/v1748519105/Erebus_New_Timesheet_For_Regular_Hours_cignif.mp4": ""} allow="autoplay" allowFullScreen ></iframe>
+                                              </div>
+                                     </div>
                                   
                                    <FaqSection />
                        </div>
